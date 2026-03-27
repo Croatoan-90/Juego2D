d2bdc12872eaf3f4f31531700d3812a2e90ac182
Nicky-kiuby <fresitaalmonacid@gmail.com>
2026-03-27 09:01:23 -0500

    Attaque

---
+++ Platform/Assets/_Scripts/PlayerShoot.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlayerShout : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+    public Transform startPos; 
+    // Start is called before the first frame update
+    void Update()
+    {
+        Shoot();
+
+    }
+
+   public void Shoot()
+   {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            GameObject bullet = Instantiate(bulletPrefab, startPos.position, startPos.rotation);
+            Destroy(bullet, 1.5f);
+        }
+       
+   }
+}
